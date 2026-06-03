b78da6730185ce1305a27a58cf9b2fd6c18e8072
Darshan S <darshans2727@gmail.com>
2026-06-03 20:04:21 +0530

    feat: add convert format page

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -8,7 +8,7 @@ import {
   SidebarTrigger
 } from '@/components/ui/sidebar'
 import Brand from './brand'
-import { Info, LucideIcon } from 'lucide-react'
+import { Info, LucideIcon, Repeat2 } from 'lucide-react'
 import Link from 'next/link'
 import { cn } from '@/lib/utils'
 import { buttonVariants } from './ui/button'
@@ -33,6 +33,17 @@ const Option = ({ Icon, name, path, isActive }: OptionProps) => {
   )
 }
 
+const options = {
+  'View metadata': {
+    icon: Info,
+    path: '/view-metadata'
+  },
+  'Convert format': {
+    icon: Repeat2,
+    path: '/convert-format'
+  }
+}
+
 export function AppSidebar() {
   const pathname = usePathname()
   return (
@@ -42,12 +53,17 @@ export function AppSidebar() {
         <Brand />
       </SidebarHeader>
       <SidebarContent className="p-2 space-y-2">
-        <Option
-          Icon={Info}
-          name="View metadata"
-          path="/view-metadata"
-          isActive={pathname === '/view-metadata'}
-        />
+        {Object.entries(options).map(([key, value]) => {
+          return (
+            <Option
+              key={key}
+              Icon={value.icon}
+              name={key}
+              path={value.path}
+              isActive={pathname === value.path}
+            />
+          )
+        })}
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
