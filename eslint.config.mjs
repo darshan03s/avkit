372c6a097f59c673e8f5de69ba7232cc2fa055ae
Darshan S <darshans2727@gmail.com>
2026-06-10 13:04:26 +0530

    chore: add eslint plugin to remove unused imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,20 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
 import nextTs from 'eslint-config-next/typescript'
+import unusedImports from 'eslint-plugin-unused-imports'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts'
-  ])
+  {
+    plugins: {
+      'unused-imports': unusedImports
+    },
+    rules: {
+      'unused-imports/no-unused-imports': 'error'
+    }
+  },
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts'])
 ])
 
 export default eslintConfig
