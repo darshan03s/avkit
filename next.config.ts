d50bd0d54e947ad37b619af155afd84f905e9e17
Darshan S <darshans2727@gmail.com>
2026-06-13 19:30:52 +0530

    chore: add allowedDevOrigins to next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,19 @@
 import type { NextConfig } from 'next'
+import os from 'node:os'
 
-const nextConfig: NextConfig = {}
+function getLocalIPs() {
+  const ips = Object.values(os.networkInterfaces())
+    .flat()
+    .filter((iface): iface is NonNullable<typeof iface> =>
+      Boolean(iface && iface.family === 'IPv4' && !iface.internal)
+    )
+    .map((iface) => iface.address)
+
+  return ips
+}
+
+const nextConfig: NextConfig = {
+  allowedDevOrigins: getLocalIPs()
+}
 
 export default nextConfig
